4b9a5cdbf2885913602f3de2a869f708d9e92147
nanayaw <nybfam175@gmail.com>
2024-04-01 16:23:24 +0100

    Added my firstproject.sh to my bash

---
+++ firstproject.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "##########################"
+echo "Start of my first project"
+echo "##########################"
+mkdir devops
+cd devops
+ls
+touch script.sh
+echo "########################"
+echo "End of my first project"
+echo "#########################"
